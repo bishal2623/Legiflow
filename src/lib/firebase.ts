3d690063190061ed172b7c3c77568fa10bae7d5d
AdityaM-IITH <adityamishra13r@gmail.com>
2026-05-22 17:12:10 +0530

    chore: lock official npm package versions and remove fallback key to resolve security flags

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: 'legiflow-p4ure',
   appId: '1:846977878595:web:e4f57e8065910d072e62f6',
   storageBucket: 'legiflow-p4ure.firebasestorage.app',
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'dummy-api-key-for-build',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'legiflow-p4ure.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '846977878595',
